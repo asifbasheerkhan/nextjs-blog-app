94a4f7a8ce32dc144c4bd2833888fd34c5c947ae
Asif <asif.khan@ksolves.com>
2025-05-06 23:00:12 +0530

    configured cors

--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,11 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 
-origins = ["http://localhost:3010/"]
+# origins = ["http://localhost:3010/"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
